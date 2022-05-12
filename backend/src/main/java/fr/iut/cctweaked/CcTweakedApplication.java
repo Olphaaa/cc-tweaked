c96a4b02a01b99e0bedcb967b92376268c483a43
Olivier BLANC <ol.blanc2000@gmail.com>
2022-05-12 13:57:54 +0200

    first commit

---
+++ backend/src/main/java/fr/iut/cctweaked/CcTweakedApplication.java
@@ -0,0 +1,13 @@
+package fr.iut.cctweaked;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CcTweakedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CcTweakedApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/fr/iut/cctweaked/CcTweakedApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.iut.cctweaked;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CcTweakedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

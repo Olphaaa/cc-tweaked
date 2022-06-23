05d9a3619e70225984e50f05d9ffcef0c258a10a
cyril-lav <cyril.laveyssiere@gmail.com>
2022-06-23 10:02:33 +0200

    adding dbref

--- backend/src/main/java/fr/iut/cctweaked/domain/Supplier.java
+++ backend/src/main/java/fr/iut/cctweaked/domain/Supplier.java
@@ -3,17 +3,14 @@ package fr.iut.cctweaked.domain;
 import fr.iut.cctweaked.domain.Location;
 import fr.iut.cctweaked.domain.enums.SupplierState;
 import fr.iut.cctweaked.domain.enums.SupplierType;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import lombok.experimental.Accessors;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
-@Getter
-@Setter
+
 @NoArgsConstructor
+@Data
 @Accessors(chain = true)
 @Document(collection = "suppliers")
 public class Supplier {

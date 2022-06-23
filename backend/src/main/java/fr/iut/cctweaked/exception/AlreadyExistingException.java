488bbf68f44739d73e8fad6f6d1f84821b2c27de
Olivier BLANC <ol.blanc2000@gmail.com>
2022-06-23 08:39:41 +0200

    [DEV] Exception management

--- backend/src/main/java/fr/iut/cctweaked/exception/AlreadyExistingException.java
+++ backend/src/main/java/fr/iut/cctweaked/exception/AlreadyExistingException.java
@@ -5,11 +5,12 @@ import org.springframework.http.HttpStatus;
 
 public class AlreadyExistingException extends Error{
 
-    public AlreadyExistingException(String message, HttpStatus status) {
-        super(message, status);
+
+    public AlreadyExistingException(String message) {
+        super(message);
     }
 
-    public AlreadyExistingException(String message, HttpStatus status, Exception base) {
-        super(message, status, base);
+    public AlreadyExistingException(String message, Exception base) {
+        super(message, base);
     }
 }
